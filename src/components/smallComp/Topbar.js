1efcb67c9f6c833d86e1393224ddcba093b61c89
AdamElhawary <mohamedadel.elhawary@yahoo.com>
2020-11-03 05:28:18 +0200

    fix

--- src/components/smallComp/Topbar.js
+++ src/components/smallComp/Topbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../styles/topbar.css';
 import toggle from '../../assets/toggle.svg';
-import {Link} from 'react-router';
+// import {Link} from 'react-router';
 export default function Topbar(props) {
     return (
 
@@ -9,7 +9,6 @@ export default function Topbar(props) {
             <div onClick={() => props.clicked()} className='sidebarToggle'>
                 <img className='toggle' src={toggle} alt='toggle' />
             </div>
-            <button><Link to={`#`} activeClassName="active">Recent Work</Link></button>
         </div>
     )
 }
